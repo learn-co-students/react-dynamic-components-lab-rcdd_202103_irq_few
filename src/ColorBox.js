044021ea2b8209016dffade55f5ca87f17476bec
Hasar <Hasar.Ali@protonmail.com>
2021-06-08 03:21:39 +0300

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-
-  state = {
-    todos: [
-
-    ]
-  }
-
   render() {
     return (
       <div className="color-box" style={{opacity: this.props.opacity}}>
